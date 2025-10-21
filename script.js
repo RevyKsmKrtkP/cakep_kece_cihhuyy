e42d6c2d9de0bc69e50403587397ee7d906c5f2d
RevyKsmKrtkP <fatikaputri09@gmail.com>
2025-10-21 13:25:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //!. tombol berdasarkan ID-nya
 const tombolSapa = document.getElementById('sapaButton');
 //2. tambahkan 'event listener' untuk aksi'clik'
-tombolSapa.addEventListener('clikc', funtion() {
+tombolSapa.addEventListener('clikc', function() {
   //3. jalankan kode ini ketik tombol di-klik
   alert("Halo! Terima kasih Sudah berkunjung!');
         }];
