5f0b269967f49087384501c5053d91721872f503
RevyKsmKrtkP <fatikaputri09@gmail.com>
2025-10-21 13:21:38 +0700

    Add click event listener to sapaButton
    Fix typo in event listener and correct function declaration.

---
+++ script.js
@@ -0,0 +1,8 @@
+//!. tombol berdasarkan ID-nya
+const tombolSapa = document.getElementById('sapaButton');
+//2. tambahkan 'event listener' untuk aksi'clik'
+tombolSapa.addEventListener('clikc', funtion() {
+  //3. jalankan kode ini ketik tombol di-klik
+  alert("Halo! Terima kasih Sudah berkunjung!');
+        }];
+                
