44ac0e1faaa968297f0bdb5a5e5e18fa9112dc5e
RevyKsmKrtkP <fatikaputri09@gmail.com>
2025-10-21 13:41:17 +0700

    Fix typo in event listener and alert message

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const tombolSapa = document.getElementById('sapaButton');
 //2. tambahkan 'event listener' untuk aksi'clik'
 tombolSapa.addEventListener('clikc', function() {
   //3. jalankan kode ini ketik tombol di-klik
-  alert("Halo! Terima kasih Sudah berkunjung!');
+  alert('Halo! Terima kasih Sudah berkunjung!');
 }];
                 
